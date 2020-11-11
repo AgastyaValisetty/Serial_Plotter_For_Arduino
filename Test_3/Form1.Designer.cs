c06b09c6ba6b164051d7afa166d5c5838cc5097b
Agastya Valisetty <59680431+AgastyaValsietty00@users.noreply.github.com>
2020-11-11 13:19:03 +0530

    Main Visual studio solution file

---
+++ Test_3/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Test_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.COMMLabel = new System.Windows.Forms.Label();
+            this.Comm = new System.Windows.Forms.ComboBox();
+            this.ConnectBtn = new System.Windows.Forms.Button();
+            this.DisconnectBtn = new System.Windows.Forms.Button();
+            this.InputField = new System.Windows.Forms.RichTextBox();
+            this.SendBtn = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.Baudrate = new System.Windows.Forms.ComboBox();
+            this.Baud = new System.Windows.Forms.Label();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.SerialMonitor = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // COMMLabel
+            // 
+            this.COMMLabel.AutoSize = true;
+            this.COMMLabel.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.COMMLabel.Location = new System.Drawing.Point(21, 27);
+            this.COMMLabel.Name = "COMMLabel";
+            this.COMMLabel.Size = new System.Drawing.Size(101, 34);
+            this.COMMLabel.TabIndex = 0;
+            this.COMMLabel.Text = "COMM:";
+            // 
+            // Comm
+            // 
+            this.Comm.FormattingEnabled = true;
+            this.Comm.Location = new System.Drawing.Point(121, 32);
+            this.Comm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Comm.Name = "Comm";
+            this.Comm.Size = new System.Drawing.Size(129, 24);
+            this.Comm.TabIndex = 1;
+            // 
+            // ConnectBtn
+            // 
+            this.ConnectBtn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectBtn.Location = new System.Drawing.Point(669, 15);
+            this.ConnectBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ConnectBtn.Name = "ConnectBtn";
+            this.ConnectBtn.Size = new System.Drawing.Size(108, 36);
+            this.ConnectBtn.TabIndex = 2;
+            this.ConnectBtn.Text = "Connect";
+            this.ConnectBtn.UseVisualStyleBackColor = true;
+            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+            // 
+            // DisconnectBtn
+            // 
+            this.DisconnectBtn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisconnectBtn.Location = new System.Drawing.Point(784, 12);
+            this.DisconnectBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DisconnectBtn.Name = "DisconnectBtn";
+            this.DisconnectBtn.Size = new System.Drawing.Size(129, 36);
+            this.DisconnectBtn.TabIndex = 3;
+            this.DisconnectBtn.Text = "Disconnect";
+            this.DisconnectBtn.UseVisualStyleBackColor = true;
+            this.DisconnectBtn.Click += new System.EventHandler(this.DisconnectBtn_Click);
+            // 
+            // InputField
+            // 
+            this.InputField.Location = new System.Drawing.Point(846, 95);
+            this.InputField.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.InputField.Name = "InputField";
+            this.InputField.Size = new System.Drawing.Size(223, 38);
+            this.InputField.TabIndex = 4;
+            this.InputField.Text = "";
+            // 
+            // SendBtn
+            // 
+            this.SendBtn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendBtn.Location = new System.Drawing.Point(939, 54);
+            this.SendBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SendBtn.Name = "SendBtn";
+            this.SendBtn.Size = new System.Drawing.Size(129, 36);
+            this.SendBtn.TabIndex = 5;
+            this.SendBtn.Text = "Send";
+            this.SendBtn.UseVisualStyleBackColor = true;
+            this.SendBtn.Click += new System.EventHandler(this.SendBtn_Click);
+            // 
+            // Baudrate
+            // 
+            this.Baudrate.FormattingEnabled = true;
+            this.Baudrate.Items.AddRange(new object[] {
+            "2400",
+            "4800",
+            "9600",
+            "57600",
+            "74880",
+            "115200",
+            "230400",
+            "250000"});
+            this.Baudrate.Location = new System.Drawing.Point(439, 27);
+            this.Baudrate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Baudrate.Name = "Baudrate";
+            this.Baudrate.Size = new System.Drawing.Size(213, 24);
+            this.Baudrate.TabIndex = 6;
+            // 
+            // Baud
+            // 
+            this.Baud.AutoSize = true;
+            this.Baud.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Baud.Location = new System.Drawing.Point(283, 22);
+            this.Baud.Name = "Baud";
+            this.Baud.Size = new System.Drawing.Size(130, 34);
+            this.Baud.TabIndex = 7;
+            this.Baud.Text = "Baudrate:";
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearBtn.Location = new System.Drawing.Point(939, 506);
+            this.ClearBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(129, 36);
+            this.ClearBtn.TabIndex = 9;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // SerialMonitor
+            // 
+            this.SerialMonitor.Location = new System.Drawing.Point(846, 137);
+            this.SerialMonitor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SerialMonitor.Multiline = true;
+            this.SerialMonitor.Name = "SerialMonitor";
+            this.SerialMonitor.Size = new System.Drawing.Size(223, 365);
+            this.SerialMonitor.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineWidth = 4;
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(27, 585);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            series4.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(807, 384);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "Plotter";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.BackColor = System.Drawing.Color.Coral;
+            this.zedGraphControl1.Location = new System.Drawing.Point(27, 83);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(807, 486);
+            this.zedGraphControl1.TabIndex = 12;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            this.zedGraphControl1.Load += new System.EventHandler(this.zedGraphControl1_Load);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(874, 575);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 417);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(852, 518);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 1031);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.SerialMonitor);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.Baud);
+            this.Controls.Add(this.Baudrate);
+            this.Controls.Add(this.SendBtn);
+            this.Controls.Add(this.InputField);
+            this.Controls.Add(this.DisconnectBtn);
+            this.Controls.Add(this.ConnectBtn);
+            this.Controls.Add(this.Comm);
+            this.Controls.Add(this.COMMLabel);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Serial Plotter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label COMMLabel;
+        private System.Windows.Forms.ComboBox Comm;
+        private System.Windows.Forms.Button ConnectBtn;
+        private System.Windows.Forms.Button DisconnectBtn;
+        private System.Windows.Forms.RichTextBox InputField;
+        private System.Windows.Forms.Button SendBtn;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox Baudrate;
+        private System.Windows.Forms.Label Baud;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.TextBox SerialMonitor;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Test_3/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using ZedGraph;
+namespace Test_3
+{
+    public partial class Form1 : Form
+    {
+        private double[] Xaxis = new double[30];
+        GraphPane GraphPane;
+        double _y= 0;
+        int NoOfTrys = 0;
+        int att = 0;
+
+        bool CanCheck = true;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            GraphPane = zedGraphControl1.GraphPane;
+            DrawSignCurve();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            string[] ports = SerialPort.GetPortNames();
+            Comm.Items.AddRange(ports);
+            Comm.SelectedIndex = 0;
+            DisconnectBtn.Enabled = false;
+
+            
+
+
+            if (serialPort1.IsOpen && CanCheck == true)
+            {
+                try
+                {
+                    _y = serialPort1.ReadByte();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+
+                }
+            }
+            if (NoOfTrys > 5)
+            {
+                CanCheck = false;
+            }
+            if (serialPort1.IsOpen)
+            {
+                _y = serialPort1.ReadByte();
+
+            }
+            else
+            {
+                return;
+            }
+        }
+        private void ConnectBtn_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            Thread masterthread;
+            masterthread = new Thread(runit);
+            masterthread.Start();
+            
+            ConnectBtn.Enabled = false;
+            DisconnectBtn.Enabled = true;
+            
+            try
+            {
+                serialPort1.PortName = Comm.Text;
+                serialPort1.BaudRate = Convert.ToInt32(Baudrate.Text);
+                serialPort1.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        string getvalue;
+
+        private void SendBtn_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                if (serialPort1.IsOpen)
+                {       
+                    serialPort1.WriteLine(InputField.Text + Environment.NewLine);
+                    //InputField.Clear();
+                    SerialMonitor.Text = serialPort1.ReadLine();
+                    InputField.Clear();
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DisconnectBtn_Click(object sender, EventArgs e)
+        {
+            ConnectBtn.Enabled = true;
+            DisconnectBtn.Enabled = false;
+
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            SerialMonitor.Clear();
+        }
+
+        /// <summary>
+        /// Marker!!!!
+        /// </summary>
+        private void DrawSignCurve()
+        {
+            PointPairList _pointPairList = new PointPairList();
+            if (serialPort1.IsOpen)
+            {
+                for (int _angle = 0; _angle <= 1360; _angle = _angle + 10)
+                {
+                    double _x = _angle;
+                    int TempYVal = 10;
+
+
+                    TempYVal = serialPort1.ReadByte();
+
+
+
+                    PointPair _pointPair = new PointPair(_x, serialPort1.ReadByte());
+                    _pointPairList.Add(_pointPair);
+                }
+            }
+         
+            
+
+            LineItem lineItem = GraphPane.AddCurve("Sine Curve", _pointPairList, Color.Red, SymbolType.None);
+            zedGraphControl1.AxisChange();
+
+
+        }
+        /// <summary>
+        /// M
+        /// </summary>
+
+        void runit()
+        {
+            while (true)
+            {
+                try
+                {
+                    if (serialPort1.IsOpen)
+                    {
+                        getvalue = serialPort1.ReadLine();
+                        //chart1.Series["Series1"].Points.AddY(getvalue);
+
+                        chart1.Invoke((MethodInvoker)(() => chart1.Series["Series1"].Points.AddXY(att, getvalue)));
+                        //chart1.Series["Series1"].Points.AddXY(,Convert.ToInt32(getvalue));
+                        //DateTime.Now.ToLongTimeString()
+
+                    }
+                }
+                catch (Exception exc)
+                {
+
+                   
+                }
+            }
+           
+           
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            att++;
+
+            if (CanCheck == true)
+            {
+                label1.Text = "true";
+            }
+            else
+            {
+                label1.Text = "false";
+            }
+
+            if (serialPort1.IsOpen && CanCheck == true)
+            {
+                _y = Convert.ToDouble(serialPort1.ReadLine());
+                
+               
+            }
+            else
+            {
+                //thinking wut to keep here //
+
+                if (_y == null)
+                {
+                    NoOfTrys++;
+
+                }
+            }
+            if (NoOfTrys > 5)
+            {
+                CanCheck = false;
+            }
+
+          //  CheckTime();
+        }
+
+
+        void CheckTime()
+        {
+         
+            
+            if (_y == null)
+            {
+                NoOfTrys++;
+
+            }
+            
+           
+        }
+
+        private void zedGraphControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CanCheck = true;
+        }
+    }
+    }
+    
+
